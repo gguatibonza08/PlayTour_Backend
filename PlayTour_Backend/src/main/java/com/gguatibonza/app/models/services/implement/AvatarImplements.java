cb55f2d172ab5175c298d7bbe30a4a4e1b46c5e2
Gian Guatibonza <gguatibonza08@gmail.com>
2019-06-26 00:03:43 -0500

    Create Controller Test

--- PlayTour_Backend/src/main/java/com/gguatibonza/app/models/services/implement/AvatarImplements.java
+++ PlayTour_Backend/src/main/java/com/gguatibonza/app/models/services/implement/AvatarImplements.java
@@ -5,8 +5,12 @@ package com.gguatibonza.app.models.services.implement;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.domain.Sort;
 
+import com.gguatibonza.app.models.daos.IAvatarDao;
 import com.gguatibonza.app.models.entities.Avatar;
 import com.gguatibonza.app.models.entities.Ruta;
 import com.gguatibonza.app.models.services.IAvatarService;
@@ -18,25 +22,32 @@ import com.gguatibonza.app.models.services.IAvatarService;
 @Service
 public class AvatarImplements implements IAvatarService {
 
+	@Autowired
+	private IAvatarDao avatarDao;
+
 	@Override
+	@Transactional(readOnly = true)
 	public List<Avatar> findAll() {
 		// TODO Auto-generated method stub
-		return null;
+		return avatarDao.findAll(Sort.by("ruta").ascending());
 	}
 
 	@Override
+	@Transactional(readOnly = true)
 	public List<Avatar> findByRuta(Ruta ruta) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
+	@Transactional(readOnly = true)
 	public Avatar findById(Long id) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
+	@Transactional(readOnly = true)
 	public boolean existById(Long id) {
 		// TODO Auto-generated method stub
 		return false;
